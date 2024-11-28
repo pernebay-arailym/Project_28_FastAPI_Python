60fd428762e5a1125adb099df8d988f60f6a39b1
pernebay-arailym <pernebayarailym@gmail.com>
2024-11-28 15:51:51 +0100

    set up parameter - first example get

--- app.py
+++ app.py
@@ -3,14 +3,27 @@ from fastapi import FastAPI
 app = FastAPI () # create api object
 
 # GET GET AND RETURN SMTH
+
+#@app.get("/") # define root
+#def home(): # created function
+#    return  {"Data": "Testing"} #python info - data
+
+#@app.get("/about")
+#def about():
+#    return {"Data": "About"}
+
 # POST SEND INFO TO THE POST/ADDING NEW USER
 # PUT 
 # DELETE
 
-@app.get("/") # define root
-def home(): # created function
-    return  {"Data": "Testing"} #python info - data
+inventory = {
+    1: {
+        "name": "Milk",
+        "price": 3.99,
+        "brand": "Regular"
+    }
+}
 
-@app.get("/about")
-def about():
-    return {"Data": "About"}
+@app.get("/get-item/{item_id}") #based on id return smth, id can be anythinght else
+def get_item(item_id: int):
+    return inventory[item_id]
