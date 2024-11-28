e5f33ceaf57940d4feb56bb2429cda43694fc337
pernebay-arailym <pernebayarailym@gmail.com>
2024-11-28 15:22:33 +0100

    defined root, created function

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 from fastapi import FastAPI
+
 app = FastAPI () # create api object
+
+# GET GET AND RETURN SMTH
+# POST SEND INFO TO THE POST/ADDING NEW USER
+# PUT 
+# DELETE
+
+@app.get("/") # define root
+def home(): # created function
+    return  {"Data": "Test"} #python info - data
