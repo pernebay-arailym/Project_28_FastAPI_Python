876d1735a36e11bf93a23cc42b5aa5191e39b86c
pernebay-arailym <pernebayarailym@gmail.com>
2024-11-29 10:39:38 +0100

    HTTPException added and replaced with errors

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path, Query
+from fastapi import FastAPI, Path, Query, HTTPException, status
 from typing import Optional
 from pydantic import BaseModel
 
@@ -39,12 +39,14 @@ def get_item(name: str=Query(None, title="Name", description="Name of item.", ma
     for item_id in inventory:
         if inventory[item_id].name== name:
             return inventory[item_id]
-    return {"Data": "Not found"}
+    #return {"Data": "Not found"}
+    raise HTTPException(status_code=404, detail="Item name not found.")
 
 @app.post("/create-item/{item_id}")
 def create_item(item_id: int, item: Item):
     if item_id in inventory:
-        return {"Error": "Item ID already exists."}
+       # return {"Error": "Item ID already exists."}
+       raise HTTPException(status_code=400, detail="Item ID already exists.")
     
     inventory[item_id]= item #{"name": item.name, "brand": item.brand, "price": item.price}
     return inventory[item_id]
@@ -52,7 +54,8 @@ def create_item(item_id: int, item: Item):
 @app.put("/update-item/{item_id}")
 def update_item(item_id: int, item: UpdateItem):
     if item_id not in inventory:
-        return {"Error": "Item ID does not exists."}
+        #return {"Error": "Item ID does not exists."}
+        raise HTTPException(status_code=404, detail="Item ID does not exist.")
     
     if item.name != None:
         inventory[item_id].name = item.name
@@ -68,6 +71,8 @@ def update_item(item_id: int, item: UpdateItem):
 @app.delete("/delete-item")
 def delete_item(item_id: int=Query(..., description="The ID of the item to delete.")):
     if item_id not in inventory:
-        return {"Error": "ID does not exists."}
+        #return {"Error": "ID does not exists."}
+        raise HTTPException(status_code=404, detail="Item ID does not exist.")
     
     del inventory[item_id]
+    return {"Success": "Item deleted!"}
